088f1feae9d17b8208b34b80e77ee98607e9aef3
lbuckley <l.b.buckley@gmail.com>
2017-02-17 16:51:10 -0800

    add Maxent comparison

--- SDMpriors.R
+++ SDMpriors.R
@@ -4,6 +4,7 @@ library(plyr)
 library(rgbif)
 library(GRaF)
 library(pROC)
+library(ROCR)
  
 #for cleaning data
 library(biogeo) #https://cran.r-project.org/web/packages/biogeo/index.html
@@ -140,6 +141,79 @@ m3 <- graf(pa_tr, train[,2:ncol(train), drop = FALSE],opt.l = TRUE, prior = lin)
 pred_df<-data.frame(predict(m3,test[,2:ncol(train), drop = FALSE]))
 print(paste("Area under ROC with prior knowledge of thermal niche : ",auc(pa_te, pred_df$posterior.mode)))
 
+#Plot response curves
+plot(m3)
+
 #--------------------------------
 # Compare SDM from above to SDM without physiological data and standard SDMS 
+
+prob <- pred_df$posterior.mode
+pred <- prediction(prob, pa_te)
+perf <- performance(pred, measure = "tpr", x.measure = "fpr")
+auc <- performance(pred, measure = "auc")
+auc <- auc@y.values[[1]]
+
+roc.data <- data.frame(fpr=unlist(perf@x.values),
+                       tpr=unlist(perf@y.values),
+                       model="GP")
+plot(roc.data$fpr,roc.data$tpr,type="l",col="red",ylab="TPR",xlab="FPR",main="ROC for GP vs MaxEnt",lwd=3.5)
+
+group_p = kfold(occ_bc, 5) # vector of group assignments splitting the Ybrev_bc into 5 groups
+group_a = kfold(bg_bc, 5) # ditto for bg_bc
+
+test = 3
+
+train_p = occ_bc[group_p!=test, c("lon","lat")]
+train_a = bg_bc[group_a!=test, c("lon","lat")]
+test_p = occ_bc[group_p==test, c("lon","lat")]
+test_a = bg_bc[group_a==test, c("lon","lat")]
+
+me = maxent(BClim, p=train_p, a=train_a)
+e = evaluate(test_p, test_a, me, BClim)
+
+print(e)
+
+#------------------------------------
+#ROC plot
+
+probs_me<-c(e@presence,e@absence)
+class_me<-c(rep(1,length(e@presence)),rep(0,length(e@absence)))
+pred_me <- prediction(probs_me, class_me)
+perf_me <- performance(pred_me, measure = "tpr", x.measure = "fpr")
+auc_me <- performance(pred_me, measure = "auc")
+auc_me <- auc_me@y.values[[1]]
+
+roc.data_me <- data.frame(fpr=unlist(perf_me@x.values),
+                          tpr=unlist(perf_me@y.values),
+                          model="ME")
+
+lines(roc.data_me$fpr,roc.data_me$tpr,type="l",col="green",lwd=3.5)
+
+legend(0.6,0.4, # places a legend at the appropriate place 
+       c("GP","MaxEnt"), # puts text in the legend
+       
+       lty=c(1,1), # gives the legend appropriate symbols (lines)
+       
+       lwd=c(2.5,2.5),col=c("red","green"))
+
+#------------------------------------
+#Maxent map
+
+pred_me = predict(me, BClim) # generate the predictions
+# make a nice plot
+plot(pred_me, 1, cex=0.5, legend=T, mar=par("mar"), xaxt="n", yaxt="n", main="Predicted presence of the species")
+map("state", xlim=c(-119, -110), ylim=c(33.5, 38), fill=F, col="cornsilk", add=T)
+
+# presence points
+points(locs$lon, locs$lat, pch=20, cex=0.5, col="darkgreen")
+# pseud-absence points
+points(bg, cex=0.5, col="darkorange3")
+
+# add axes
+axis(1,las=1)
+axis(2,las=1)
+
+# restore the box around the map
+box()
+
 
