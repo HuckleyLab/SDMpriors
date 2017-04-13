d2e6075c17c3abbb109ec8d53e56e9afa6fea1e5
Tony Cannistra <tony.cannistra@gmail.com>
2017-04-13 10:58:39 -0700

    added species selection code

---
+++ select_species.R
@@ -0,0 +1,12 @@
+library(rgbif)
+
+res = read.csv("all_species.csv", header=FALSE)
+
+get.centroid = function(spec){
+	occs = occ_data(scientificName = spec, limit=1000, minimal=TRUE)$data
+	cent.lat = mean(occs$decimalLatitude)
+	cent.lon = mean(occs$decimalLongitude)
+	return(c(cent.lat, cent.lon))
+}
+
+lapply(res[10:20,2], get.centroid)
