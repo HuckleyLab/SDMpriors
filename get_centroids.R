59976998b59fc4ab049294f4f922cff08b322431
Tony Cannistra <tony.cannistra@gmail.com>
2017-04-14 12:12:07 -0700

    refactored centroid scripts

---
+++ get_centroids.R
@@ -0,0 +1,19 @@
+library(rgbif)
+
+args = commandArgs(trailingOnly=TRUE)
+
+res = read.csv(args[1], header=FALSE)
+
+get.centroid = function(spec){
+	occs = occ_data(scientificName = spec, limit=1000, minimal=TRUE)$data
+	if (is.null(occs)){
+		print(paste("No occurrence data found for ", spec))
+	}
+	occs = occs[which(!is.na(occs$"decimalLongitude") & !is.na(occs$"decimalLatitude")),]
+	cent.lat = mean(occs$decimalLatitude)
+	cent.lon = mean(occs$decimalLongitude)
+	print(paste(spec, " finished", sep=""))
+	return(c(cent.lat, cent.lon))
+}
+
+write.table(lapply(res[,2], get.centroid), args[2], sep=",")
